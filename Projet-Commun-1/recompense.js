cddbb48cc5f486d0b930de26d7142c49f631bd3b
Jsegur <92443084+Jsegur@users.noreply.github.com>
2022-04-07 21:33:57 +0200

    Add files via upload
    update recompense.js

--- Projet-Commun-1/recompense.js
+++ Projet-Commun-1/recompense.js
@@ -1,10 +1,10 @@
 import Data from "./utilities/dataHandling.js";
 import * as apiUtilities from "./utilities/apiUtilities.js";
+import { PlayerData } from "./main.js";
+import { PlayerData } from "./data/minions.json";
 
-const attractionsData = await apiUtilities.getAttractions();
 const minionsData = await apiUtilities.getMinions();
-let DataObject = new Data();
-let PlayerData = DataObject.getData();
+
 let Currency = document.getElementById("Currency-Counter");
 
 console.log(PlayerData)
@@ -25,9 +25,17 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+let i = 0
+arraydemesrecompenses = {"money" : 0, "bronze" : 0, "silver" : 0, "or" : 0, "gemme": 0}
+minions[i].awards = {"money" : 0, "bronze" : 10, "silver" : 24, "or" : 50, "gemme": 0}
 
-let x = PlayerData.Army['Army'+data.id];
-let y = console.log(minions.json[1].money);
+for (const [idArmy,nbAmry] of Object.entries(PlayerData.Army)){
+    i++
+    nbAmry
 
-let z = x * y
+    for (const [key,value] of Object.entries(minions[i].awards)){
+
+        
+        arraydemesrecompenses[key] += value * nbAmry    
+    }
+}
