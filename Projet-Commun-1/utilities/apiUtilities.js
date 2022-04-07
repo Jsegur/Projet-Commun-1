e016297082651f87ab209f964c5c711c4cb91055
Jsegur <92443084+Jsegur@users.noreply.github.com>
2022-04-07 06:31:24 +0200

    Add files via upload
    ajout apiUtilities.js

---
+++ Projet-Commun-1/utilities/apiUtilities.js
@@ -0,0 +1,31 @@
+import * as Global from "../utilities/global.js";
+
+export async function getAttractions(){
+    var url = `${Global.LINK}/Projet-Commun-1/src/data/main.json`;
+
+    const response = await fetch(url, {
+        method: 'GET', 
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type':'application/json'
+        }
+    }
+    )
+    const attractions = await response.json();
+    return attractions
+}
+
+export async function getMinions(){
+    var url = `${Global.LINK}/Projet-Commun-1/src/data/minions.json`;
+
+    const response = await fetch(url, {
+        method: 'GET', 
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type':'application/json'
+        }
+    }
+    )
+    const minions = await response.json();
+    return minions
+}
